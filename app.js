88971550b2f04e85447321cb438cfbf76a7c0bfb
Amnestic <72355018+Amnestic@users.noreply.github.com>
2021-08-14 21:59:03 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const discRpc = require('discord-rpc');
+const rpc = new discRpc.Client({
+    transport: 'ipc'
+});
+rpc.on('ready', () => {
+    rpc.setActivity({
+        details: this.version,
+        state: 'Random State Info',
+        startTimestamp: Date.now(),
+        smallImageKey: 'Random info',
+        smallImageText: 'Random info',
+        largeImageKey: 'Random info',
+        largeImageText: 'Random info',
+        instance: true
+    });
+});
+rpc.login({
+    clientId: '876183898311233537',
+});
