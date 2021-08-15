e0551b5f3c8090b03b74aefa954a2cff64077913
Amnestic <72355018+Amnestic@users.noreply.github.com>
2021-08-15 23:33:13 +0200

    ✨ Added ability to add buttons

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+  
 // Template made by Amnestic
 
 const discRpc = require('discord-rpc');
@@ -10,11 +11,17 @@ rpc.on('ready', () => {
         details: this.version,
         state: 'Description', // Just a basic description!
         startTimestamp: Date.now(),
-        smallImageKey: 'icon_name',                 // Insert a name of your image that you have set on your app. | Displays a image
+        smallImageKey: 'icon',                 // Insert a name of your image that you have set on your app. | Displays a image
         smallImageText: 'image_hover_text',         // Text that will display when you hover over the image.
         largeImageKey: 'icon_name', 
         largeImageText: 'image_hover_text', 
+        buttons: [ 
+            { label: 'Button Name', url: 'Your URL goes here' } // <--- Button  | To add buttons just copy the line and replace the fields. For ex. { label: 'Button Name', url: 'Your URL goes here' } { label: 'Button Name', url: 'Your URL goes here' }
+            
+        ],
         instance: true
+      
+        
     });
 });
 rpc.login({
